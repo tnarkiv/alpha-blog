1a4486aed4f1a818982fabeb7dc8492604a3e429
Vikrant <vikrant2099ad@gmail.com>
2016-04-13 16:06:56 +0000

    Edit, Index, Show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,27 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -11,9 +31,11 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+  
   def show
     @article = Article.find(params[:id])
   end
+  
   private
     def article_params
       params.require(:article).permit(:title,:description)
