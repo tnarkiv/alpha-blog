7a4d391b8d875f56ac66eebb7e58d8ef628f5bca
Vikrant <vikrant2099ad@gmail.com>
2016-04-15 08:03:53 +0000

    Add gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,10 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 
   private
   def user_params
